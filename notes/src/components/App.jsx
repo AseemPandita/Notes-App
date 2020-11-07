9df445690fdf6a3d171986809b383d5c4cfa093f
Aseem Panditam <aseempandita@live.com>
2020-11-07 23:53:38 +0530

    Added search functionality

--- notes/src/components/App.jsx
+++ notes/src/components/App.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import NotesData from "../notesData";
 
 
+
 function App(){
+
+    const [searchTerm, getSearchTerm] = useState("");
+    
+function filterResult(event){
+    getSearchTerm(event.target.value);
+}
+
+
     return (
         <div>
             <Header />
-            {NotesData.map(noteData => (
+
+            <div className="search">
+                <input type = "text" name = "searchTerm" placeholder = "Search..." onChange={filterResult}/>
+            </div>
+
+
+            {NotesData.filter(noteData => noteData.title.toLowerCase().includes(searchTerm.toLowerCase())).map(note => 
                 <Note 
-                    key={noteData.id} 
-                    title={noteData.title} 
-                    content={noteData.content} />
-                )
+                        key={note.id} 
+                        title={note.title} 
+                        content={note.content} />
             )}
+
+
             <Footer />
         </div>
         )
